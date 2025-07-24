053f9e0e2ace4d0383e4fc938fce3b8100e7594a
kryssie3290 <christabeladamu2@gmail.com>
2025-07-24 09:11:46 -0700

    friends

---
+++ friend.py
@@ -0,0 +1,18 @@
+friends = ["Aisha", "Daniel", "Esther", "John", "Mary", "Paul", "Ruth"]
+print(friends)
+friends.insert(4, "kemi")
+print(friends)
+friends.remove(friends[1])
+print(friends)
+friends[0] = "Aisha.M"
+print(friends)
+friends.insert(7, "Zainab")
+print(friends)
+final_list = ["Aisha.M", "Esther", "john"]
+print(final_list)
+print(len(friends[0:5]))
+
+
+
+
+
