4dd0ac40e1e7278eb1779add97f7d0b2d8a8f9e3
kryssie3290 <christabeladamu2@gmail.com>
2025-07-24 08:41:15 -0700

    money

--- money.py
+++ money.py
@@ -1,7 +1,7 @@
 money = [1000, 1200, 800, 1500, 1100]
 total_money = money[0]+money[1]+money[2]+money[3]+money[4]
 print(total_money)
-money.append(2, 1000)
+money[2] =  1000
 print(money)
 money[::-1]
 print(money)
