00dc3b0d4ef239d26cfa7611d260b1016aaddd75
kryssie3290 <christabeladamu2@gmail.com>
2025-07-24 07:57:58 -0700

    meals

---
+++ meals.py
@@ -0,0 +1,9 @@
+meals = ["gwote", "masa", "tuwon acha", "fura da nono", "kunu", "miyan taushe"]
+print(meals)
+meals[3] = "miyan taushe"
+print(meals)
+meals.remove(meals[1])
+print(meals)
+meals.insert(6, "fura da nono")
+print(meals)
+
